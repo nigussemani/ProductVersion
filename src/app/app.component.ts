a8c38ff2aabf2fb17ac6e4108d17c409e882130e
Nigus <nigus.semani@gmail.com>
2020-09-04 09:08:15 -0500

    Minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {TeamCityBuildNumberServiceTsService} from "./team-city-build-number-service.ts.service";
 import {environment} from "../environments/environment";
 
 
@@ -11,22 +10,17 @@ import {environment} from "../environments/environment";
 export class AppComponent implements OnInit {
   title = 'ProductVersion';
   version = '1.0.0.0';
-  constructor(private buildService: TeamCityBuildNumberServiceTsService) {
+  constructor() {
   }
 
   ngOnInit() {
     this.getVersion();
   }
   getVersion(): any {
-    var storedVeriosn = localStorage.getItem('version');
-    if (storedVeriosn !== null || storedVeriosn !== undefined)
-      return storedVeriosn;
-
-    let buildNumber: number;
     let version = `${environment.major}.${environment.minor}.${environment.patch}.${environment.buildNumber}`;
     this.version = version;
     console.log("version="+version);
-    localStorage.setItem("version", version)
+
     return version;
   }
   getBuildNumber(){
